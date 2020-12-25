ba9394e6bb5b5ac382603b9e81745d194e5df037
chanyeong <cyjo9603@naver.com>
2020-12-25 18:31:35 +0900

    Chore : eslint 및 prettier 설정
    - airbnb-base 규칙 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,9 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
+    'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
