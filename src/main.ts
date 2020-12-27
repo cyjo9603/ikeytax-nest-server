38e7a66af3c8997d01ac1603502e59bb73b447df
chanyeong <cyjo9603@naver.com>
2020-12-27 22:06:54 +0900

    Chore : server logger적용
    - server logger로 morgan 모듈 설치 및 적용

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
 import { AppModule } from '@/app.module';
+
+const prod = process.env.NODE_ENV === 'production';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  if (prod) {
+    app.use(logger('combined'));
+  } else {
+    app.use(logger('dev'));
+  }
 
   await app.listen(3000);
 }
