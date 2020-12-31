0b89bf3472e38b13aa457296e841f1052b591002
chanyeong <cyjo9603@naver.com>
2020-12-31 14:14:15 +0900

    Feat : getChat Query 구현
    - getChat Query 구현

--- src/chat/chat.resolver.ts
+++ src/chat/chat.resolver.ts
@@ -1,4 +1,15 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { GetChatResponse } from '@/graphql';
+import { ChatService } from './chat.service';
 
 @Resolver()
-export class ChatResolver {}
+export class ChatResolver {
+  constructor(private chatService: ChatService) {}
+
+  @Query(() => GetChatResponse)
+  async getChat(@Args('chatId') orderId: string) {
+    const chats = await this.chatService.getChats(orderId);
+
+    return { result: 'success', chats };
+  }
+}
