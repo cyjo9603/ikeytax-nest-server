eafbe5936c61eafee47fa7cb73fd6ebea2b58608
chanyeong <cyjo9603@naver.com>
2020-12-31 14:13:17 +0900

    Feat : 채팅 조회 서비스 로직 구현
    - 채팅 조회 서비스 로직 구현

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Order, OrderDocument } from '@models/order.model';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(@InjectModel(Order.name) private orderModel: Model<OrderDocument>) {}
+
+  async getChats(orderId: string) {
+    const order = await this.orderModel.findById(orderId, 'chat');
+    return order?.get('chat');
+  }
+}
