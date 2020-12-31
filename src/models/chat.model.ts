f8b951e91df92eb75e4cd06f47e6c34df31066e3
chanyeong <cyjo9603@naver.com>
2020-12-31 14:30:45 +0900

    Chore : Chat model의 timestamp 변경
    - timestamp를 createdAt 하나만 생성

--- src/models/chat.model.ts
+++ src/models/chat.model.ts
@@ -1,13 +1,16 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema({ _id: false, timestamps: true })
+@Schema({ _id: false })
 export class Chat {
   @Prop({ required: true, enum: ['small', 'middle', 'large'] })
   writer: string;
 
   @Prop({ required: true })
   content: string;
+
+  @Prop({ default: Date.now() })
+  createdAt?: Date | number;
 }
 
 export type ChatDocument = Chat & Document;
