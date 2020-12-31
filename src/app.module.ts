e924f2ff127d6eab9e49e987df08d68384bec1d7
chanyeong <cyjo9603@naver.com>
2020-12-31 14:03:08 +0900

    Feat : Chat 모듈 생성
    - 채팅 관련 기능을 추가하기 위해 Chat 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 import { PubsubModule } from '@configs/pubsub.module';
 import { UserModule } from './user/user.module';
-import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { ChatModule } from './chat/chat.module';
 
 const prod = process.env.NODE_ENV === 'production';
 
@@ -38,6 +39,7 @@ const prod = process.env.NODE_ENV === 'production';
     UserModule,
     AuthModule,
     OrderModule,
+    ChatModule,
   ],
 })
 export class AppModule {}
