c8a6c6338d16183372b7ed38f832de6c8640a764
chanyeong <cyjo9603@naver.com>
2020-12-31 14:32:05 +0900

    Feat : createChat Mutation 구현
    - createChat Mutation 구현
    - 채팅 구독 기능 틀만 적용 (아직 채팅 구독 기능을 구현하지 않음)

--- README.md
+++ README.md
@@ -37,6 +37,6 @@
 
 #### chat
 
-- [ ] createChat
-- [ ] getChat
+- [x] createChat
+- [x] getChat
 - [ ] subChat
--- src/chat/chat.resolver.ts
+++ src/chat/chat.resolver.ts
@@ -1,15 +1,36 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
-import { GetChatResponse } from '@/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { PubSub } from 'apollo-server-express';
+import { CreateChatResponse, GetChatResponse } from '@/graphql';
+import { UseGuards, Inject } from '@nestjs/common';
+
+import { PUB_SUB, NEW_CHAT } from '@configs/config.constants';
+import { CurrentUser } from '@user/decorators/currentUser';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
 import { ChatService } from './chat.service';
 
 @Resolver()
 export class ChatResolver {
-  constructor(private chatService: ChatService) {}
+  constructor(private chatService: ChatService, @Inject(PUB_SUB) private readonly pubsub: PubSub) {}
 
   @Query(() => GetChatResponse)
   async getChat(@Args('chatId') orderId: string) {
     const chats = await this.chatService.getChats(orderId);
 
     return { result: 'success', chats };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => CreateChatResponse)
+  async createChat(
+    @CurrentUser() user,
+    @Args('chatId') orderId: string,
+    @Args('content') content: string,
+  ) {
+    await this.chatService.add(user.id, orderId, content);
+    const chat = { writer: user.id, content };
+
+    this.pubsub.publish(NEW_CHAT, { subChat: { result: 'success', chat, orderId } });
+
+    return { result: 'success', chat };
   }
 }
