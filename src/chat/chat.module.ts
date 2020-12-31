363af32d574f364e1402b4811684ebf0ba025487
chanyeong <cyjo9603@naver.com>
2020-12-31 14:06:05 +0900

    Chore : Chat 모듈에 mongoose order 모듈 연결
    - Chat 모듈에 mongoose order 모듈 연결
    - order 모듈안에 chat 데이터가 들어가 있으므로 연결함

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from '@models/order.model';
+
 import { ChatResolver } from './chat.resolver';
 import { ChatService } from './chat.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+    ]),
+  ],
   providers: [ChatResolver, ChatService],
 })
 export class ChatModule {}
